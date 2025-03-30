9a12363b0b9d398a78cf4594b2144d7716a11569
Sarah <yyaya85@gmail.com>
2025-03-30 03:30:28 +0200

    update security.tf

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
 # Generate a new SSH key pair
-resource "tls_private_key" "pk" {
+resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
@@ -7,9 +7,9 @@ resource "tls_private_key" "pk" {
 # Create and download the key pair
 resource "aws_key_pair" "main" {
   key_name   = "mykey"
-  public_key = tls_private_key.pk.public_key_openssh
+  public_key = tls_private_key.ssh_key.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./mykey.pem && chmod 400 ./mykey.pem"
+    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > ./mykey.pem && chmod 400 ./mykey.pem"
   }
 }
