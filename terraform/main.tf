54b80f82517d9f5ad2868346cdac5364b55908c7
mostafa-sharkawy <83875029+mostafa-sharkawy@users.noreply.github.com>
2025-04-12 11:26:00 +0200

    Add Terra files

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,8 +62,22 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    from_port   = 8081
-    to_port     = 8081
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8088
+    to_port     = 8088
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- terraform/security.tf
+++ terraform/security.tf
@@ -10,6 +10,7 @@ resource "aws_key_pair" "main" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > ./mykey.pem && chmod 400 ./mykey.pem"
+    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > ../Ansible/mykey.pem && chmod 400 ../Ansible/mykey.pem"
   }
+  
 }
