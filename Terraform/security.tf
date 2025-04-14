c09bd07f68a106e4f6b15db057aca3051d4793f1
mostafa-sharkawy <83875029+mostafa-sharkawy@users.noreply.github.com>
2025-04-14 17:42:55 +0200

    Update security.tf

--- Terraform/security.tf
+++ Terraform/security.tf
@@ -6,8 +6,12 @@ resource "tls_private_key" "ssh_key" {
 
 # Create and download the key pair
 resource "aws_key_pair" "main" {
-  key_name   = "mykey"
+  key_name   = "mykey${timestamp()}"
   public_key = tls_private_key.ssh_key.public_key_openssh
+  
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.ssh_key.private_key_pem}' > ../Ansible/mykey.pem && chmod 400 ../Ansible/mykey.pem"
